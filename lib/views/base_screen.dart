82d1a22244cdef5957c3b902f54ff3f3459115b1
sackoba <badrasacko11@gmail.com>
2025-05-17 19:24:51 +0000

    fix icon color in base_screen.dart

--- lib/views/base_screen.dart
+++ lib/views/base_screen.dart
@@ -20,9 +20,7 @@ class ScaffoldWithNavbar extends StatelessWidget {
       navigationShell.currentIndex == 1
           ? Iconsax.calendar
           : Iconsax.calendar_1_copy,
-      navigationShell.currentIndex == 2
-          ? Icons.done_all_sharp
-          : Icons.done_all_sharp,
+      Icons.done_all_sharp,
       navigationShell.currentIndex == 3 ? Iconsax.user : Iconsax.user_copy,
     ];
 
