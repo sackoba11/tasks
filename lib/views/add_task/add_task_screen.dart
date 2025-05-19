ef6f067f5b6a637cc13948333615ba2f3e7a6ba2
sackoba <badrasacko11@gmail.com>
2025-05-19 20:28:27 +0000

    add color picker to add_task_screen.dart

--- lib/views/add_task/add_task_screen.dart
+++ lib/views/add_task/add_task_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flex_color_picker/flex_color_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:tasks/utils/constants/enums.dart';
@@ -96,19 +97,22 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
                   keyboardType: TextInputType.multiline,
                 ),
 
-                // Positioned(
-                //   bottom: TSizes.spaceBtwSections - 4,
-                //   left: TSizes.defaultSpace,
-                //   right: TSizes.defaultSpace,
-                //   child: Column(
-                //     crossAxisAlignment: CrossAxisAlignment.center,
-                //     mainAxisAlignment: MainAxisAlignment.start,
-                //     children: [
-                //       const SafeArea(child: SizedBox(height: 15)),
-                //       const SafeArea(child: SizedBox(height: 15)),
-                //     ],
-                //   ),
-                // ),
+                Positioned(
+                  bottom: TSizes.spaceBtwSections - 40,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      const SafeArea(child: SizedBox(height: 15)),
+                      Container(
+                        height: 40,
+                        width: 40,
+                        color: dialogPickerColor,
+                      ),
+                      const SafeArea(child: SizedBox(height: 15)),
+                    ],
+                  ),
+                ),
                 CustomStepper(),
                 const SizedBox(height: TSizes.spaceBtwSections),
                 const SizedBox(height: TSizes.spaceBtwSections),
@@ -124,11 +128,87 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
             child: CustomElevatedButton(
               title: 'Ajouter la tâche',
               onPressed: () {
-                context.pop();
+                colorPickerDialog();
+                // context.pop();
               },
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  static const Color guidePrimary = Color(0xFF6200EE);
+  static const Color guidePrimaryVariant = Color(0xFF3700B3);
+  static const Color guideSecondary = Color(0xFF03DAC6);
+  static const Color guideSecondaryVariant = Color(0xFF018786);
+  static const Color guideError = Color(0xFFB00020);
+  static const Color guideErrorDark = Color(0xFFCF6679);
+  static const Color blueBlues = Color(0xFF174378);
+  final Map<ColorSwatch<Object>, String>
+  colorsNameMap = <ColorSwatch<Object>, String>{
+    ColorTools.createPrimarySwatch(guidePrimary): 'Guide Purple',
+    ColorTools.createPrimarySwatch(guidePrimaryVariant): 'Guide Purple Variant',
+    ColorTools.createAccentSwatch(guideSecondary): 'Guide Teal',
+    ColorTools.createAccentSwatch(guideSecondaryVariant): 'Guide Teal Variant',
+    ColorTools.createPrimarySwatch(guideError): 'Guide Error',
+    ColorTools.createPrimarySwatch(guideErrorDark): 'Guide Error Dark',
+    ColorTools.createPrimarySwatch(blueBlues): 'Blue blues',
+  };
+  Color dialogPickerColor = Colors.red;
+  Future<bool> colorPickerDialog() async {
+    return ColorPicker(
+      color: dialogPickerColor,
+      onColorChanged: (Color color) {
+        setState(() {
+          dialogPickerColor = color;
+        });
+      },
+      width: 40,
+      height: 40,
+      borderRadius: 4,
+      spacing: 5,
+      runSpacing: 5,
+      wheelDiameter: 155,
+      heading: Text(
+        'Select color',
+        style: Theme.of(context).textTheme.titleMedium,
+      ),
+      subheading: Text(
+        'Select color shade',
+        style: Theme.of(context).textTheme.titleMedium,
+      ),
+      wheelSubheading: Text(
+        'Selected color and its shades',
+        style: Theme.of(context).textTheme.titleMedium,
+      ),
+      showMaterialName: true,
+      showColorName: true,
+      showColorCode: true,
+      copyPasteBehavior: const ColorPickerCopyPasteBehavior(
+        longPressMenu: true,
+      ),
+      materialNameTextStyle: Theme.of(context).textTheme.bodySmall,
+      colorNameTextStyle: Theme.of(context).textTheme.bodySmall,
+      colorCodeTextStyle: Theme.of(context).textTheme.bodyMedium,
+      colorCodePrefixStyle: Theme.of(context).textTheme.bodySmall,
+      selectedPickerTypeColor: Theme.of(context).colorScheme.primary,
+      pickersEnabled: const <ColorPickerType, bool>{
+        ColorPickerType.both: true,
+        ColorPickerType.primary: true,
+        ColorPickerType.accent: true,
+        ColorPickerType.bw: true,
+        ColorPickerType.custom: true,
+        ColorPickerType.wheel: true,
+      },
+      // customColorSwatchesAndNames: colorsNameMap,
+    ).showPickerDialog(
+      context,
+      actionsPadding: const EdgeInsets.all(16),
+      constraints: const BoxConstraints(
+        minHeight: 480,
+        minWidth: 300,
+        maxWidth: 320,
       ),
     );
   }
